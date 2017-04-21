021f914bacf9319940196125bd4aef10101c29fb
spixooze <s_simon_s@hotmail.com>
2017-04-21 12:05:06 +0200

    Add Boilerplate och första version av databaschema efter workshop

---
+++ models/database.js
@@ -0,0 +1,17 @@
+const pg = require('pg');
+const fs = require('fs');
+
+//const database = require('./database.sql').toString();
+
+const databaseSql = fs.readFileSync('models/database.sql').toString();
+
+const connectionString = process.env.DATABASE_URL || 'postgres://ag7949:6n9id9en@pgserver.mah.se:5432/ag7949';
+
+const client = new pg.Client(connectionString);
+client.connect();
+const query = client.query(databaseSql, (err, res) => {
+    if (err) {
+        console.log(err);
+    }
+});
+query.on('end', () => { client.end(); });
---
+++ models/database.sql
@@ -0,0 +1,78 @@
+-- Databasschemat;
+
+CREATE TABLE Category
+(
+    id SERIAL PRIMARY KEY, -- Ifall man vill ändra Category title efterhand;
+    name varchar(40) NOT NULL UNIQUE
+);
+
+CREATE TABLE Subcategory
+(
+    id SERIAL PRIMARY KEY,
+    name varchar(40) NOT NULL UNIQUE,
+    parent_category int NOT NULL,
+
+    FOREIGN KEY (parent_category) REFERENCES Category(id)
+);
+
+CREATE TABLE Article
+(
+    id SERIAL PRIMARY KEY,
+    title varchar(40) NOT NULL,
+    content TEXT NOT NULL,
+    subcategory int NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+
+    FOREIGN KEY (subcategory) REFERENCES Subcategory(id)
+);
+
+CREATE TABLE Author
+(
+    socialsecuritynumber int NOT NULL PRIMARY KEY,
+    firstname varchar(30) NOT NULL,
+    surname varchar(30) NOT NULL,
+    comment text
+);
+
+CREATE TABLE Article_author
+(
+    article_id int,
+    socialsecuritynumber int,
+
+    PRIMARY KEY (article_id, socialsecuritynumber),
+
+    FOREIGN KEY (article_id) REFERENCES Article(id),
+    FOREIGN KEY (socialsecuritynumber) REFERENCES Author(socialsecuritynumber)
+);
+
+CREATE TABLE Article_comment
+(
+    id SERIAL PRIMARY KEY,
+    author varchar(40) NOT NULL,
+    comment varchar(160) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    article_id int NOT NULL,
+
+    FOREIGN KEY (article_id) REFERENCES Article(id)
+);
+
+CREATE TABLE Image
+(
+    id SERIAL PRIMARY KEY,
+    image_ref varchar NOT NULL,
+    subcategory int NOT NULL,
+    alt_text varchar(30) NOT NULL,
+
+    FOREIGN KEY (subcategory) REFERENCES Subcategory(id)
+);
+
+CREATE TABLE Article_image
+(
+    article_id int NOT NULL,
+    image_id int NOT NULL,
+    article_image_text varchar(60),
+    
+    PRIMARY KEY (article_id, image_id),
+    FOREIGN KEY (article_id) REFERENCES Article(id),
+    FOREIGN KEY (image_id) REFERENCES Image(id)
+);
