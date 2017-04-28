61a0f0eaaa9ef8086bc5cf963f2f503000a8be00
spixooze <s_simon_s@hotmail.com>
2017-04-28 17:34:38 +0200

    Add fix

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ var webpack = require('webpack');
 module.exports = {
     entry: [
         'script!jquery/dist/jquery.min.js',
-        // 'script!foundation-sites/dist/foundation.min.js',
         './app/app.jsx'
     ],
     externals: {
